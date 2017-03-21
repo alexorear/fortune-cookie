8d7f552b1887b67c8b2f84ce9b1c7e3b3d488e6a
Alex Orear <alex.orear@gmail.com>
2017-03-21 17:44:06 -0500

    Set up fortune cookie rand number

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class MainHandler(webapp2.RequestHandler):
         header = "<h1>Fortune Cookie</h1>"
         lucky_number = random.randint(1, 100)
         number_sentence = "Your lucky number: " + str(lucky_number)
+        number_paragraph = '<p>' + number_sentence + '</p>'
         self.response.write(header + number_sentence)
 
 app = webapp2.WSGIApplication([
