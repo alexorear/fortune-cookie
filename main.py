fb3681bed3b93c249cd56ea28ca409da287fe114
Alex Orear <alex.orear@gmail.com>
2017-03-21 19:27:17 -0500

    Added random fortune function

--- main.py
+++ main.py
@@ -17,13 +17,34 @@
 import webapp2
 import random
 
+def getRandomFortune():
+    #List of possible fortunes
+    fortunes = [
+        "I see much code in your future",
+        "Consider eating more fortune cookies",
+        "You have tamed the mighty Python, now you must free it onto the Great Spider's Web"
+    ]
+    #Randomly select and return a fortunes
+    return random.choice(fortunes)
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         header = "<h1>Fortune Cookie</h1>"
-        lucky_number = random.randint(1, 100)
-        number_sentence = "Your lucky number: " + str(lucky_number)
+
+        fortune = '<strong>' + getRandomFortune() + '</strong>'
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_paragraph = '<p>' + fortune_sentence + '</p>'
+
+        lucky_number = '<strong>' + str(random.randint(1, 100)) + '</strong>'
+        number_sentence = "Your lucky number: " + lucky_number
         number_paragraph = '<p>' + number_sentence + '</p>'
-        self.response.write(header + number_sentence)
+
+        cookie_again_button = '<a href="."><button>Another Cookie Plz</button></a>'
+
+        content = header + fortune_paragraph + number_paragraph + cookie_again_button
+
+        self.response.write(content)
+
 
 routes = [
     ('/', MainHandler),
